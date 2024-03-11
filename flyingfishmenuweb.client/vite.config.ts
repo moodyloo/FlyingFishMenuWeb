5dcbbd840be58355f35a1955e2469279837010c9
Ming Wu <qi_qiwu@hotmail.com>
2024-03-11 23:20:37 +0000

    change vite config ts

--- flyingfishmenuweb.client/vite.config.ts
+++ flyingfishmenuweb.client/vite.config.ts
@@ -1,3 +1,4 @@
+/** 
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
@@ -55,3 +56,7 @@ export default defineConfig({
         }
     }
 })
+*/
+export default {
+    // config options
+}
