b4344c73c2274d9f5340895aff6d04224ff360f4
Ming Wu <qi_qiwu@hotmail.com>
2024-03-12 19:08:33 +0000

    add swagger doc

--- FlyingFishMenuWeb.Server/Program.cs
+++ FlyingFishMenuWeb.Server/Program.cs
@@ -7,7 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Minimal API",
+        Version = "v1"
+    });
+});
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(
