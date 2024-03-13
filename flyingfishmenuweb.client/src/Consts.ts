01cad03ced1533240e7771f6137b307da52f61c6
Ming Wu <qi_qiwu@hotmail.com>
2024-03-13 13:44:34 +0000

    change api url for deployment

--- flyingfishmenuweb.client/src/Consts.ts
+++ flyingfishmenuweb.client/src/Consts.ts
@@ -1,5 +1,6 @@
 //Web api base url
-export const url: string = 'http://localhost:5055/api/MenuItem' as const;
+//export const url: string = 'http://localhost:5055/api/MenuItem' as const; //for local host
+export const url: string = 'https://flyingfishmenuapi.azurewebsites.net' as const; //for deployment
 
 //Currency Symbols
 export const CurrencySymbolDictionary: { [iso: string]: string; } = {
