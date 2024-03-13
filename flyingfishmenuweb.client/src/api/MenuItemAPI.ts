4fe63515ea1354438380c737f083f6ed20eeb923
Ming Wu <qi_qiwu@hotmail.com>
2024-03-13 14:01:49 +0000

    add headers to api cell

--- flyingfishmenuweb.client/src/api/MenuItemAPI.ts
+++ flyingfishmenuweb.client/src/api/MenuItemAPI.ts
@@ -5,7 +5,9 @@ import { url } from '../Consts.ts';
 export async function GetAllMenuItem() : Promise<MenuItem[]|[]> {
     try
     {
-        const response = await axios.get<MenuItem[]>(url + '/api/MenuItem');
+        const response = await axios.get<MenuItem[]>(url + '/api/MenuItem', {
+            headers: { 'Access-Control-Allow-Origin': 'https://jolly-cliff-06b700b03.5.azurestaticapps.net' }
+        });
         return response.data;
     }
     catch (error) {
