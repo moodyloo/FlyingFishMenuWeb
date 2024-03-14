26b4612371759195d31c5b3ce2785484274aca5c
Ming Wu <qi_qiwu@hotmail.com>
2024-03-14 12:38:22 +0000

    add environmental check for url

--- flyingfishmenuweb.client/src/MenuItemCard.tsx
+++ flyingfishmenuweb.client/src/MenuItemCard.tsx
@@ -19,11 +19,6 @@ export default function MenuItemCard(props: Props) {
 
     const countryISO = useAppSelector((state) => state.countryISO.value);
     const currencySymbol = CurrencySymbolDictionary[countryISO]!;
-    /**
-    const menuItemPrices = props.menuItem.priceDetails.map((itemPrice, i) => {
-        return <Dropdown.Item onSelect id={`Price-${i}-${props.menuItem.id}`}>{`${itemPrice.price.toFixed(2)} - ${itemPrice.size}`}</Dropdown.Item>;
-    });
-    */
 
     const itemPrices = props.menuItem.priceDetails.map((itemPrice, i) => {
         return <ListGroup.Item id={`Price-${i}-${props.menuItem.id}`}>{`${itemPrice.size} - ${currencySymbol}${itemPrice.price.toFixed(2)}`}</ListGroup.Item>;
@@ -32,27 +27,14 @@ export default function MenuItemCard(props: Props) {
     return (
         <>
             <Card style={cardStyle}>
-                {/**<Card.Title>
-                    <Button style={buttonStyle} variant="primary">+</Button>
-                </Card.Title>*/}
                 <Card.Body style={cardBodyStyle}>
                     <Card.Title>{props.menuItem.name}</Card.Title>
                     <Card.Text>{props.menuItem.description}</Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    {/**
-                        <Form.Control
-                        type="text"
-                        id={props.menuItem.id + "-textbox"}
-                        disabled
-                    />*/ }
                     <ListGroup>
                         {itemPrices}
                     </ListGroup>
-                    {/**
-                        <DropdownButton id="dropdown-basic-button" title="">
-                            {menuItemPrices}
-                        </DropdownButton>*/}
                 </Card.Body>
             </Card>
         </>
