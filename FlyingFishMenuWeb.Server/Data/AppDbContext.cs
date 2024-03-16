c6f14159d73ae5948f4d3b1cda6faf1463d9223f
Ming Wu <qi_qiwu@hotmail.com>
2024-03-16 16:07:21 +0000

    remove appsettings.development.json from remote repository

---
+++ FlyingFishMenuWeb.Server/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlyingFishMenuWeb.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public AppDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
