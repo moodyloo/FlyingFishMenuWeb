5e1ed0db78ceba804bf11685de97a91ed618c215
Ming Wu <qi_qiwu@hotmail.com>
2024-08-29 20:38:14 +0100

    temporarily disable chinese from menu

---
+++ FlyingFishMenuWeb.Server/DisableAllChinese.sql
@@ -0,0 +1,9 @@
+﻿UPDATE MenuItemVariants
+SET IsUnavailable = true
+WHERE Id IN (
+    SELECT miv.Id
+    FROM MenuItemVariants miv
+    INNER JOIN MenuItems mi ON miv.MenuItem_Id = mi.Id
+    INNER JOIN ItemCategories ic ON mi.Category_Id = ic.Id
+    WHERE ic.Id IN (2, 3, 5, 6, 7, 8, 9, 10, 11, 15)
+);
--- flyingfishmenuweb.client/src/AllergyModal.tsx
+++ flyingfishmenuweb.client/src/AllergyModal.tsx
@@ -9,12 +9,10 @@ export default function AllergyModal(props: Props) {
     const [visible, setVisible] = useState(props.visible);
 
     useEffect(() => {
+
         const popup_shown = sessionStorage.getItem("popup_shown");
         if (popup_shown != "1") {
             sessionStorage.setItem("popup_shown", "1");
-        }
-        else {
-            setVisible(false);
         }
     }, []);
 
@@ -22,14 +20,19 @@ export default function AllergyModal(props: Props) {
         <>
             <Modal centered keyboard show={visible} onHide={() => setVisible(false)}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Food Allergies And Intolerance</Modal.Title>
+                    {/*<Modal.Title>Food Allergies And Intolerance</Modal.Title>*/}
+                    <Modal.Title>Important Notice</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <p>Please note that our Chinese food items are temporarily unavailable.</p>
+                    <p>We apologize for any inconvenience and appreciate your understanding.</p>
+                </Modal.Body>
+                {/*<Modal.Body>
                     <p>Some of our dishes contain:</p>
                     <p><b>Celery, Cereals Containing Gluten, Crustaceans, Egg, Fish, Lupin, Milk, Mustard, Molluscs, Peanuts, Sesame Seeds, Soya, Nuts, Sulphur Dioxide</b></p>
                     <p>which some people may have a reaction to.</p>
                     <p>Please inform our staff before ordering if you suffer from any food allergies. We will do our best to advise you.</p>
-                </Modal.Body>
+                </Modal.Body>*/}
 
                 <Modal.Footer>
                     <Button variant="secondary" onClick={() => setVisible(false)}>Close</Button>
